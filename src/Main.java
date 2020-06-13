8d51e201809d95605d8a55830f5f0581e2b046ea
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-13 15:49:22 -0300

    Leitura das variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		int X = 0, Y = 0;
+		
+		X = sc.nextInt();
+		Y = sc.nextInt();
+		
+		sc.close();
 	}
 
 }
